1c7c5ce15e4627a6fb00dc20a8362bc09964e538
Tom <tdenottelander@gmail.com>
2016-10-18 12:52:12 +0200

    joe

--- Project3/myFunctions.h
+++ Project3/myFunctions.h
@@ -1,7 +1,10 @@
 void myFunction1(Mesh);
 void myFunction2(Mesh);
 void myFunction3(Mesh);
+void generateEdges(Mesh m);
 void myFunction4(Mesh);
 void myFunction5(Mesh);
+void myFunction6(Mesh);
+void myFunctionS(Mesh);
 
 void draw(Mesh);
